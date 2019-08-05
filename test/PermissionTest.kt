a80b82d972b194a1e2a5ff88ce2163f85ddefd4e
Simon Schäffner <simon.schaeffner@googlemail.com>
2019-08-05 16:34:40 +0200

    add test for adding user permission

---
+++ test/PermissionTest.kt
@@ -0,0 +1,102 @@
+package cloud.fabx
+
+import cloud.fabx.dto.*
+import cloud.fabx.model.ToolState
+import cloud.fabx.model.ToolType
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.setBody
+import io.ktor.server.testing.withTestApplication
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class PermissionTest {
+    @Test
+    fun testPermissions() {
+        withTestApplication({ module(testing = true) }) {
+
+            val mapper = jacksonObjectMapper()
+
+            // CREATE USER
+            handleRequest(HttpMethod.Post, "/api/user") {
+                setBody(mapper.writeValueAsString(
+                    NewUserDto(
+                        "New User 1",
+                        "newUserWikiName",
+                        "123456",
+                        "aabbccdd"
+                    )
+                ))
+                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            }.apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+            }
+
+            // CREATE DEVICE
+            handleRequest(HttpMethod.Post, "/api/device") {
+                setBody(mapper.writeValueAsString(
+                    NewDeviceDto(
+                        "New Device 1",
+                        "aaffeeaaffee",
+                        "newSecret",
+                        "http://bgurl"
+                    )
+                ))
+                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            }.apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+            }
+
+            // CREATE TOOL
+            handleRequest(HttpMethod.Post, "/api/tool") {
+                setBody(
+                    mapper.writeValueAsString(
+                        NewToolDto(
+                            1,
+                            "New Tool 1",
+                            0,
+                            ToolType.UNLOCK,
+                            ToolState.GOOD,
+                            "http://wikiurl"
+                        )
+                    )
+                )
+                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            }.apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+            }
+
+            // ADD PERMISSION
+            handleRequest(HttpMethod.Post, "/api/user/1/permissions") {
+                setBody(
+                    mapper.writeValueAsString(
+                        NewUserPermissionDto(
+                            1,
+                            1
+                        )
+                    )
+                )
+                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            }.apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+            }
+
+            handleRequest(HttpMethod.Get, "/api/user/1").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertTrue(response.content!!.isNotEmpty())
+
+                val userDto = mapper.readValue<UserDto>(response.content!!)
+
+                assertEquals(userDto.id, 1)
+                assertEquals(userDto.permissions.size, 1)
+                assertEquals(userDto.permissions[0].deviceId, 1)
+            }
+        }
+    }
+}
