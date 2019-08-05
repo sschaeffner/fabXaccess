b836fb5441544a5ec4e916a5b9b0d949de4d2a9b
Simon Schäffner <simon.schaeffner@googlemail.com>
2019-08-05 20:23:03 +0200

    refactor routing

--- src/Application.kt
+++ src/Application.kt
@@ -1,7 +1,6 @@
 package cloud.fabx
 
 import cloud.fabx.db.DbHandler
-import cloud.fabx.dto.*
 import cloud.fabx.model.*
 import cloud.fabx.service.DeviceService
 import cloud.fabx.service.ToolService
@@ -13,7 +12,6 @@ import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.features.*
 import io.ktor.jackson.jackson
-import io.ktor.request.receive
 import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.SizedCollection
 import org.jetbrains.exposed.sql.StdOutSqlLogger
@@ -47,137 +45,8 @@ fun Application.module(testing: Boolean = false) {
             call.respond(HttpStatusCode.BadRequest, cause.localizedMessage)
         }
     }
-
-    routing {
-        route("/api") {
-            route("/user") {
-                get("") {
-                    call.respond(userService.getAllUsers())
-                }
-
-                get("/{id}") {
-                    val user: UserDto? = call.parameters["id"]?.toInt()?.let {
-                        userService.getUserById(it)
-                    }
-
-                    if (user != null) {
-                        call.respond(user)
-                    } else {
-                        call.respond(HttpStatusCode.NotFound)
-                    }
-                }
-
-                put("/{id}") {
-                    val user: UserDto? = call.parameters["id"]?.toInt()?.let {
-                        userService.getUserById(it)
-                    }
-
-                    if (user == null) {
-                        call.respond(HttpStatusCode.NotFound)
-                    } else {
-                        val editUser = call.receive<EditUserDto>()
-                        userService.editUser(user.id, editUser)
-
-                        call.respond(HttpStatusCode.OK)
-                    }
-                }
-
-                post("/{id}/permissions") {
-                    val userPermission = call.receive<UserPermissionDto>()
-                    userService.addUserPermission(userPermission.userId, userPermission.toolId)
-                    call.respond(HttpStatusCode.OK)
-                }
-
-                delete("/{id}/permissions/{toolId}") {
-                    val userId = call.parameters["id"]!!.toInt()
-                    val toolId = call.parameters["toolId"]!!.toInt()
-
-                    userService.removeUserPermission(userId, toolId)
-                    call.respond(HttpStatusCode.OK)
-                }
-
-                post("") {
-                    val user = call.receive<NewUserDto>()
-                    call.respond(userService.createNewUser(user))
-                }
-            }
-
-            route("/device") {
-                get("") {
-                    call.respond(deviceService.getAllDevices())
-                }
-
-                get("/{id}") {
-                    val device: DeviceDto? = call.parameters["id"]?.toInt()?.let {
-                        deviceService.getDeviceById(it)
-                    }
-
-                    if (device != null) {
-                        call.respond(device)
-                    } else {
-                        call.respond(HttpStatusCode.NotFound)
-                    }
-                }
-
-                put("/{id}") {
-                    val device: DeviceDto? = call.parameters["id"]?.toInt()?.let {
-                        deviceService.getDeviceById(it)
-                    }
-
-                    if (device == null) {
-                        call.respond(HttpStatusCode.NotFound)
-                    } else {
-                        val editDevice = call.receive<EditDeviceDto>()
-                        deviceService.editDevice(device.id, editDevice)
-
-                        call.respond(HttpStatusCode.OK)
-                    }
-                }
-
-                post("") {
-                    val device = call.receive<NewDeviceDto>()
-                    call.respond(deviceService.createNewDevice(device))
-                }
-            }
-
-            route("/tool") {
-                get("") {
-                    call.respond(toolService.getAllTools())
-                }
-
-                get("/{id}") {
-                    val tool: ToolDto? = call.parameters["id"]?.toInt()?.let {
-                        toolService.getToolById(it)
-                    }
-
-                    if (tool != null) {
-                        call.respond(tool)
-                    } else {
-                        call.respond(HttpStatusCode.NotFound)
-                    }
-                }
-
-                put("/{id}") {
-                    val tool: ToolDto? = call.parameters["id"]?.toInt()?.let {
-                        toolService.getToolById(it)
-                    }
-
-                    if (tool == null) {
-                        call.respond(HttpStatusCode.NotFound)
-                    } else {
-                        val editTool = call.receive<EditToolDto>()
-                        toolService.editTool(tool.id, editTool)
-
-                        call.respond(HttpStatusCode.OK)
-                    }
-                }
-
-                post("") {
-                    val tool = call.receive<NewToolDto>()
-                    call.respond(toolService.createNewTool(tool))
-                }
-            }
-        }
+    install(Routing) {
+        routes()
     }
 }
 
